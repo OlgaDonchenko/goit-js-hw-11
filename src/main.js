141913fce7603633f910bee3f39b71c3391e33df
OlgaDonchenko <n0931606045@gmail.com>
2024-03-11 19:32:30 +0200

    homework11

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-import iconRejected from './img/octagon.png'
+import { getPictures, iconRejected } from './js/pixabay-api';
+import { createMarkup, initializeLightbox } from './js/render-functions';
 
 const formSearch = document.querySelector('.form-search');
 const listImages = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
+const loader = document.querySelector('.lds-heart');
 
 loader.style.display = 'none';
 
@@ -55,12 +53,7 @@ function onSearch(event) {
             }
 
             listImages.insertAdjacentHTML('afterbegin', createMarkup(data.hits));
-            const refreshPage = new SimpleLightbox('.gallery a', {
-                captions: true,
-                captionsData: 'alt',
-                captionDelay: 250,
-            });
-            refreshPage.refresh();
+            initializeLightbox();
 
             formSearch.reset();
         })
@@ -79,58 +72,4 @@ function onSearch(event) {
             });
         })
         .finally(() => loader.style.display = 'none');
-}
-
-function getPictures(name) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const KEY = '42458918-8f01ea81f4ffacec8edc4f5cf';
-
-    const searchParams = new URLSearchParams({
-        key: KEY,
-        q: name,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-    })
-
-    return fetch(`${BASE_URL}?${searchParams}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            return response.json();
-        })
-}
-
-function createMarkup(arr) {
-    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-        `<li class="gallery-item">
-              <a class="gallery-link" href="${largeImageURL}">
-                <img
-                  class="gallery-image"
-                  src="${webformatURL}"
-                  alt="${tags}"
-                  width="360"
-                />
-              </a>
-              <div class="thumb-block">
-                <div class="block">
-                  <h2 class="title">Likes</h2>
-                  <p class="amount">${likes}</p>
-                </div>
-                <div class="block">
-                  <h2 class="title">Views</h2>
-                  <p class="amount">${views}</p>
-                </div>
-                <div class="block">
-                  <h2 class="title">Comments</h2>
-                  <p class="amount">${comments}</p>
-                </div>
-                <div class="block">
-                  <h2 class="title">Downloads</h2>
-                  <p class="amount">${downloads}</p>
-                </div>
-              </div>
-            </li>`)
-        .join('');
 }
